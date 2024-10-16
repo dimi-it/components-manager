f494b1b946735708d30536dab40b19718f7876fe
DIMITRI MASETTA <S306130@studenti.polito.it>
2024-10-16 19:49:54 +0200

    feat: added console tests

---
+++ ConsoleApp/Tests.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+using DBManager.DTOs.Components;
+using DBManager.DTOs.Components.Resistors;
+using DBManager.Repositories;
+using DBManager.Repositories.Components.Resistor;
+
+namespace ConsoleApp;
+
+public static class Tests
+{
+    public static async Task Run1(string connectionString, string dbName)
+    {
+        Console.WriteLine("RUN 1");
+        Resistor_SMDRepository repository = new Resistor_SMDRepository(new MongoConnection(connectionString, dbName));
+        Resistor_SMD r1 = new Resistor_SMD()
+        {
+            Vendor = "Io",
+            VendorProductCode = "code2",
+            Footprint = "0805",
+            Resistance = new ComponentParameter<double>(1000.0, "1kΩ"),
+            Tolerance = new ComponentParameter<string>("+=1")
+        };
+        await repository.CreateAsync(r1);
+        Console.WriteLine(JsonSerializer.Serialize((await repository.GetAllAsync()).ToList()));
+    }
+}
