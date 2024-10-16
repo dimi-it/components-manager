aab2d6c0fa8f283f5c41bfd502bc71ce57cc52d7
DIMITRI MASETTA <S306130@studenti.polito.it>
2024-10-16 19:49:17 +0200

    feat: new util type MutualExclusiveTuple

---
+++ DBManager/Util/MutualExclusiveTuple.cs
@@ -0,0 +1,26 @@
+﻿namespace DBManager.Util;
+
+public class MutualExclusiveTuple<T, U>
+{
+    private readonly T? _first;
+    private readonly U? _second; 
+    private readonly bool _isFirstNull = false;
+    
+    public MutualExclusiveTuple(T? first, U? second)
+    {
+        if (first is null && second is null)
+        {
+            throw new InvalidOperationException("Only one of the value can be null");
+        }
+
+        _first = first;
+        _second = second;
+        _isFirstNull = first is null ? true : false;
+    }
+
+    public bool IsFirst() => _isFirstNull;
+    public bool IsSecond() => _isFirstNull;
+
+    public T GetFirst() => _first ?? throw new NullReferenceException("First is null");
+    public U GetSecond() => _second ?? throw new NullReferenceException("Second is null");
+}
