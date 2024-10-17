ea13fcac9551e74b29f7ce4cb2646d98b7bf96db
DIMITRI MASETTA <S306130@studenti.polito.it>
2024-10-17 16:23:23 +0200

    feat: Capacitor model added

---
+++ DBManager/DTOs/Components/Capacitors/Capacitor_MLCC_SMD.cs
@@ -0,0 +1,24 @@
+﻿namespace DBManager.DTOs.Components.Capacitors;
+
+public class Capacitor_MLCC_SMD: Component, ICapacitor
+{
+    public required ComponentParameter<string> Footprint { get; init; }
+    public required ComponentParameter<double> Capacitance { get; init; }
+    public required ComponentParameter<string> Tolerance { get; init; }
+    public required ComponentParameter<double> RatedVoltage { get; init; }
+    public ComponentParameter<string>? TemperatureCoefficient { get; init; }
+
+    public Capacitor_MLCC_SMD(Component? baseComponent = null)
+    {
+        //initialize the base component parameters, if a base component is provided
+        if (baseComponent != null)
+        {
+            base.InitByInstance(baseComponent); 
+        }
+    }
+
+    public void SetDefaultName()
+    {
+        SetName($"Capacitor MLCC SMD {Footprint} {Capacitance} {RatedVoltage} {Tolerance}");
+    }
+}
---
+++ DBManager/DTOs/Components/Capacitors/ICapacitor.cs
@@ -0,0 +1,10 @@
+﻿namespace DBManager.DTOs.Components.Capacitors;
+
+public interface ICapacitor: IComponent
+{
+    ComponentParameter<string> Footprint { get; }
+    ComponentParameter<double> Capacitance { get; }
+    ComponentParameter<string> Tolerance { get; }
+    ComponentParameter<double> RatedVoltage { get; }
+    void SetDefaultName();
+}
